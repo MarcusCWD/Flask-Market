a19300ca2a59358f6206c5fc8fc4bfbaa8e2afda
Marcus <97861729+MarcusCWD@users.noreply.github.com>
2024-01-15 13:23:28 +0800

    updated

--- market.py
+++ market.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from sqlalchemy.orm import Session
-from model import engine, Base, User, Post
+from model import engine, Base, Item
+import os
 
 app = Flask(__name__)
 
@@ -11,46 +12,26 @@ Base.metadata.create_all(bind=engine)
 db = Session(bind=engine)
 
 
-# Route to create a new user
-@app.route('/create_user', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-
-    new_user = User(username=username, email=email, password=password)
-    db.add(new_user)
-    db.commit()
-
-    return jsonify({'message': 'User created successfully'}), 201
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
 
 
-# Route to get all users
-@app.route('/get_users', methods=['GET'])
-def get_users():
-    users = db.query(User).all()
-    user_list = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
-    return jsonify(user_list)
-
-
-# Route to create a new post for a user
-@app.route('/create_post', methods=['POST'])
-def create_post():
-    data = request.get_json()
-    user_id = data.get('user_id')
-    title = data.get('title')
-    body = data.get('body')
-
-    user = db.query(User).filter_by(id=user_id).first()
-    if user:
-        new_post = Post(title=title, body=body, author=user)
-        db.add(new_post)
-        db.commit()
-        return jsonify({'message': 'Post created successfully'}), 201
-    else:
-        return jsonify({'error': 'User not found'}), 404
+@app.route('/market', methods=['GET'])
+def market_page():
+    items = db.query(Item).all()
+    # item_list = [{'id': item.id,
+    #               'name': item.name,
+    #               'price': item.price,
+    #               'barcode': item.barcode,
+    #               'description': item.barcode
+    #               } for item in items]
+    # item_list = [{key: value for key, value in item.__dict__.items() if not key.startswith('_')}
+    #               for item in items]
+    db.close()
+    return render_template('market.html', items=items)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.environ.get('APP_PORT'))
--- model.py
+++ model.py
@@ -6,30 +6,27 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-# Replace the connection parameters with your MariaDB credentials and database name
-DATABASE_URL = os.environ.get('DATABASE_URL')
+# Connection to Database i.e 'mysql+mysqlconnector://root@localhost:3307/market.db'
+database_connector = 'mysql+mysqlconnector'
+DATABASE_URL = (database_connector + '://'
+                + os.environ.get('DATABASE_USER')
+                + '@'
+                + os.environ.get('DATABASE_ADDRESS')
+                + ':'
+                + os.environ.get('DATABASE_PORT')
+                + '/'
+                + os.environ.get('DATABASE_NAME'))
+
 engine = create_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users'
+### Declare all tables and relationships here below ###
+class Item(Base):
+    __tablename__ = 'items'
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)  # Specify a length, for example, 50 characters
-    email = Column(String(100), unique=True, index=True)    # Adjust the length as needed
-    password = Column(String(255))  # Specify a length, for example, 255 characters
-
-    posts = relationship('Post', back_populates='author')
-
-class Post(Base):
-    __tablename__ = 'posts'
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(255), index=True)  # Adjust the length as needed
-    body = Column(String(1000))  # Specify a length, for example, 1000 characters
-    author_id = Column(Integer, ForeignKey('users.id'))
-
-    author = relationship('User', back_populates='posts')
-
-Base.metadata.create_all(bind=engine)
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(length=30), nullable=False, unique=True)
+    price = Column(Integer(), nullable=False)
+    barcode = Column(String(length=12), nullable=False, unique=True)
+    description = Column(String(length=1024), nullable=False, unique=True)
